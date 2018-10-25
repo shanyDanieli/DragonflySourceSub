53c5fccdd5a961e14e8040a7d7fea9a618ce7497
Shany Danieli <shany.danieli@yale.edu>
2018-10-25 12:58:18 -0400

    in the process of writing a function that calcualtes the scaling factor between the model and the dragonfly image, using SExtractor

--- sourcesub.py
+++ sourcesub.py
@@ -3,7 +3,7 @@
 """starsub.py -- subtract the stars from a dragonfly image using a high resolution image (e.g. cfht) to identify and model stars
 
 Usage:
-    starsub [-h] [-v] [-u] [-m] [-l LOCATION] [-p PSF] <dragonflyimagename> <highresimagename> <paramfile>
+    starsub [-h] [-v] [-u] [-m] [-l LOCATION] [-p PSF] [-s LOC] <dragonflyimagename> <highresimagename> <paramfile>
 
 Options:
     -h, --help                                  Show this screen
@@ -15,6 +15,7 @@ Options:
     -l LOCATION, --locpsf LOCATION              Directory where the psf code is [default: /Users/deblokhorst/Documents/Dragonfly/git/]
 
     -p PSF, --givenpsf PSF                      PSF  name  [default: ./psf/_psf_g.fits]
+    -s LOC, --sexloc LOC                    	Location of SExtractor executable						[default: /usr/local/bin/sex]
 
 Examples:
 
@@ -25,8 +26,9 @@ import docopt
 import pyraf
 from pyraf import iraf
 import numpy as np
-from astropy.io import fits
+from astropy.io import fits, ascii
 from astropy.nddata.utils import block_replicate
+from matplotlib import pyplot as plt
 from iraf import stsdas
 from iraf import analysis
 try:
@@ -40,6 +42,182 @@ from iraf import fconvolve
 import os 
 import sys
 import subprocess
+import re
+
+
+
+sextractor_params = """NUMBER
+FLUX_AUTO 
+FLUXERR_AUTO
+X_IMAGE
+Y_IMAGE
+X_WORLD
+Y_WORLD
+ELLIPTICITY
+FWHM_IMAGE
+THETA_IMAGE
+"""
+
+sextractor_config = """
+    ANALYSIS_THRESH 3
+        BACK_FILTERSIZE 3
+        BACKPHOTO_TYPE GLOBAL
+        BACK_SIZE 128
+        CATALOG_NAME test.cat
+        CATALOG_TYPE ASCII_HEAD
+        CLEAN Y
+        CLEAN_PARAM 1.
+        DEBLEND_MINCONT .005
+        DEBLEND_NTHRESH 32
+        DETECT_MINAREA 5
+        DETECT_THRESH {detect_thresh}
+        DETECT_TYPE CCD
+        FILTER Y
+        FILTER_NAME {filter_name}
+        FLAG_IMAGE flag.fits
+        GAIN 1.0
+        MAG_GAMMA 4.
+        MAG_ZEROPOINT 0.0
+        MASK_TYPE CORRECT
+        MEMORY_BUFSIZE 4096
+        MEMORY_OBJSTACK 30000
+        MEMORY_PIXSTACK 3000000
+        PARAMETERS_NAME {parameters_name}
+        PHOT_APERTURES 3
+        PHOT_AUTOPARAMS 2.5, 3.5
+        PIXEL_SCALE {pixel_scale}
+        SATUR_LEVEL 50000.
+        SEEING_FWHM 2.5
+        STARNNW_NAME {starnnw_name}
+        VERBOSE_TYPE {verbose_type}
+"""
+
+
+default_conv = """CONV NORM
+# 3x3 ``all-ground'' convolution mask with FWHM = 2 pixels. 
+1 2 1
+2 4 2
+1 2 1
+"""
+
+default_nnw = """NNW
+# Neural Network Weights for the SExtractor star/galaxy classifier (V1.3)
+# inputs:       9 for profile parameters + 1 for seeing.
+# outputs:      ``Stellarity index'' (0.0 to 1.0)
+# Seeing FWHM range: from 0.025 to 5.5'' (images must have 1.5 < FWHM < 5 pixels)
+# Optimized for Moffat profiles with 2<= beta <= 4.
+ 3 10 10  1
+
+-1.56604e+00 -2.48265e+00 -1.44564e+00 -1.24675e+00 -9.44913e-01 -5.22453e-01  4.61342e-02  8.31957e-01  2.15505e+00  2.64769e-01
+ 3.03477e+00  2.69561e+00  3.16188e+00  3.34497e+00  3.51885e+00  3.65570e+00  3.74856e+00  3.84541e+00  4.22811e+00  3.27734e+00
+
+-3.22480e-01 -2.12804e+00  6.50750e-01 -1.11242e+00 -1.40683e+00 -1.55944e+00 -1.84558e+00 -1.18946e-01  5.52395e-01 -4.36564e-01 -5.30052e+00
+ 4.62594e-01 -3.29127e+00  1.10950e+00 -6.01857e-01  1.29492e-01  1.42290e+00  2.90741e+00  2.44058e+00 -9.19118e-01  8.42851e-01 -4.69824e+00
+-2.57424e+00  8.96469e-01  8.34775e-01  2.18845e+00  2.46526e+00  8.60878e-02 -6.88080e-01 -1.33623e-02  9.30403e-02  1.64942e+00 -1.01231e+00
+ 4.81041e+00  1.53747e+00 -1.12216e+00 -3.16008e+00 -1.67404e+00 -1.75767e+00 -1.29310e+00  5.59549e-01  8.08468e-01 -1.01592e-02 -7.54052e+00
+ 1.01933e+01 -2.09484e+01 -1.07426e+00  9.87912e-01  6.05210e-01 -6.04535e-02 -5.87826e-01 -7.94117e-01 -4.89190e-01 -8.12710e-02 -2.07067e+01
+-5.31793e+00  7.94240e+00 -4.64165e+00 -4.37436e+00 -1.55417e+00  7.54368e-01  1.09608e+00  1.45967e+00  1.62946e+00 -1.01301e+00  1.13514e-01
+ 2.20336e-01  1.70056e+00 -5.20105e-01 -4.28330e-01  1.57258e-03 -3.36502e-01 -8.18568e-02 -7.16163e+00  8.23195e+00 -1.71561e-02 -1.13749e+01
+ 3.75075e+00  7.25399e+00 -1.75325e+00 -2.68814e+00 -3.71128e+00 -4.62933e+00 -2.13747e+00 -1.89186e-01  1.29122e+00 -7.49380e-01  6.71712e-01
+-8.41923e-01  4.64997e+00  5.65808e-01 -3.08277e-01 -1.01687e+00  1.73127e-01 -8.92130e-01  1.89044e+00 -2.75543e-01 -7.72828e-01  5.36745e-01
+-3.65598e+00  7.56997e+00 -3.76373e+00 -1.74542e+00 -1.37540e-01 -5.55400e-01 -1.59195e-01  1.27910e-01  1.91906e+00  1.42119e+00 -4.35502e+00
+
+-1.70059e+00 -3.65695e+00  1.22367e+00 -5.74367e-01 -3.29571e+00  2.46316e+00  5.22353e+00  2.42038e+00  1.22919e+00 -9.22250e-01 -2.32028e+00
+
+ 0.00000e+00
+ 1.00000e+00
+"""
+
+
+def mkdirp(dirpath):
+    if not os.path.isdir(dirpath):
+        os.makedirs(dirpath)
+    return None
+
+def print_verbose_string(asverbose):
+    print >> sys.stderr,'VERBOSE: %s' % asverbose
+
+def cleanit(fname,iama='file'):
+    if iama == 'file':
+        if os.path.isfile(fname):
+            os.remove(fname)
+    elif iama == 'dir':
+        shutil.rmtree(fname)
+    return None
+
+def run_SExtractor(imagename,detect_thresh=10):
+    'Names and storage directory of required config files'
+    sextractor_config_name = './pipetmp/scamp.sex'
+    params_name = './pipetmp/scamp.param'
+    nnw_name = './pipetmp/default.nnw'
+    conv_name = './pipetmp/default.conv'
+    mkdirp('./pipetmp')
+
+    'Output ascii catalog name and seg map name'
+    catname = re.sub('.fits','.cat',imagename)
+    segname = re.sub('.fits','_seg.fits',imagename)
+
+    if verbose:
+        verbose_type = 'NORMAL'
+    else:
+        verbose_type = 'QUIET'
+
+    'Stick content in config files'
+    configs = zip([sextractor_config_name,params_name,conv_name,nnw_name],[sextractor_config,sextractor_params,default_conv,default_nnw])
+    for fname,fcontent in configs:
+        fout = open(fname,'w')
+
+        if 'scamp.sex' in fname:
+            fout.write(fcontent.format(filter_name=conv_name,parameters_name=params_name,
+                                       starnnw_name=nnw_name,verbose_type=verbose_type,
+                                       detect_thresh=detect_thresh,pixel_scale=2.5))
+        else:
+            fout.write(fcontent)
+
+        fout.close()
+
+    if verbose:
+        print_verbose_string('SExtracting... ')
+
+    SExtract_command = sexloc +' -c {config} -CATALOG_NAME {catalog} -CHECKIMAGE_TYPE SEGMENTATION -CHECKIMAGE_NAME {seg} {image}'.format(config=sextractor_config_name,catalog=catname,seg=segname,image=imagename)
+    subprocess.call(SExtract_command,shell=True)
+
+    'Clean up'
+    for fname in [sextractor_config_name,params_name,nnw_name,conv_name]:
+        cleanit(fname)
+
+    return catname
+
+
+
+def getphotosc(model,df):
+	catname = run_SExtractor(df,detect_thresh=3)
+	cat = ascii.read(catname)
+	flux = cat['FLUX_AUTO']
+	x = cat['X_IMAGE']
+	y = cat['Y_IMAGE']
+	median_flux = np.median(np.asarray(flux))
+
+	x_cut = []
+	y_cut = []
+	flux_cut = []
+
+	for i in range(len(flux)):
+		if flux[i] < median_flux
+
+
+	print(median)
+
+	plt.hist(flux,bins=1000)
+	plt.axvline(x=median,c='r')
+	plt.show()
+	plt.close()
+	quit()
+
+
+
+	return photosc
+
 
 def writeFITS(im,header,saveas):
     if os.path.isfile(saveas):
@@ -226,6 +404,7 @@ if __name__ == '__main__':
     # Non-mandatory options with arguments
     psf = arguments['--givenpsf']
     locpsf = arguments['--locpsf']
+    sexloc = arguments['--sexloc']
     
     if verbose:
         print arguments
@@ -267,6 +446,7 @@ if __name__ == '__main__':
     # photosc = 1/(10**((zp_df-zp_cfht)/(-2.5))*pix_size_df**2/(pix_size_cfht**2))
 
 
+    # Reading parameters from the user
     user_parameters = []
     use_or_not = []
     with open(parameters_file,'r') as i:
@@ -291,18 +471,12 @@ if __name__ == '__main__':
     width_cfhtsm = parameters_to_use[4]
     width_mask = parameters_to_use[5]
 
-    '''
-    ##### Go through the column with 0/1 to determine which to pass the defaults instead of user values
-    default_dict = {'photosc':None,'upperlim':0.04,'lowerlim':0.005,'shifts':[0,0],
-                    'width_cfhtsm':0, 'width_mask':1.5}
-    
-    ##### something like this?
-    for default_param,file_param,file_column in default_dict,file_params,file_columns:
-        if file_column == 0:
-            param_dict[param] = default_param
-        else:
-            param_dict[param] = file_param
-    '''
+
+
+    getphotosc(df_image,df_image)
+   
+    quit()
+
         
     prep(df_image,hi_res_image,width_mask=width_mask)
     subract(df_image,psf,shifts=shifts,photosc=photosc,width_cfhtsm=width_cfhtsm,upperlim=upperlim,lowerlim=lowerlim)
